29b042009c71cc01e2af503c8a2ebc404158d52a
cece93150 <cece93150@hotmail.fr>
2022-09-08 10:51:31 +0200

    create mapview and navigation bar

---
+++ app/src/main/java/com/projettest/go4lunch/ui/MainActivity.java
@@ -0,0 +1,56 @@
+package com.projettest.go4lunch.ui;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.projettest.go4lunch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TOOLBAR
+        toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+
+        //Handle fragments in bottom navigation view
+        bottomNavBarFragmentsManagement();
+
+        //Display Map fragment by default
+        displayFragment(new MapsFragment());
+
+    }
+
+    public void displayFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void bottomNavBarFragmentsManagement() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation:
+                    toolbar.setTitle("I'm Hungry");
+                    MainActivity.this.displayFragment(new MapsFragment());
+                    return true;
+            }
+            return false;
+        });
+    }
+}
+
+
